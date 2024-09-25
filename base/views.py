adf38bde2ac9d2545ba58ebf9bec3b4d2e16602d
Abel Varghese <abelv0538@gmail.com>
2024-09-24 21:58:36 -0400

    5:11:50 - Edit User Account Page

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 # Create your views here.
@@ -112,15 +112,16 @@ def createRoom(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == 'POST':
-        #form = RoomForm(request.POST)
+        form = RoomForm(request.POST)
         topic_name = request.POST.get('topic')
         topic, created = Topic.objects.get_or_create(name = topic_name) 
+        
 
         Room.objects.create(
-            host=request.user,
-            topic = topic,
-            name = request.POST.get('name'),
-            description = request.POST.get('description'),
+           host=request.user,
+           topic = topic,
+           name = request.POST.get('name'),
+           description = request.POST.get('description'),
         )
         return redirect('home')
 
@@ -131,8 +132,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    topics = Topic.object.all()
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('Your are not allowed here!')
     
@@ -176,3 +176,17 @@ def deleteMessage(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html',{'form': form})
+
